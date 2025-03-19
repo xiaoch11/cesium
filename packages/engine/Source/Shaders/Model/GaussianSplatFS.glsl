8e3d70a7dbd1125f11b2b49f69612d5eef8f20fb
xiaoch11 <434682672@qq.com>
2025-03-19 11:15:27 +0800

    gaussian splat support debugColorizeTiles on

--- packages/engine/Source/Shaders/Model/GaussianSplatFS.glsl
+++ packages/engine/Source/Shaders/Model/GaussianSplatFS.glsl
@@ -4,6 +4,15 @@ void gaussianSplatStage(inout vec4 color, in ProcessedAttributes attributes) {
         discard;
     mediump float scale = 4.0;
     mediump float B = exp(-A * scale) * (v_splatColor.a);
-    color = vec4(v_splatColor.rgb * B, B);
+    
+    vec3 splatColor = v_splatColor.rgb;
+
+    #ifdef HAS_MODEL_COLOR
+    splatColor = mix(splatColor, model_color.rgb, model_colorBlend);
+    float highlight = ceil(model_colorBlend);
+    splatColor *= mix(model_color.rgb, vec3(1.0), highlight);
+    #endif
+
+    color = vec4(splatColor * B, B);
 }
 
